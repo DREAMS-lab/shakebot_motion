c0da6d6f431fff37afaf77c4749f34dd414205b9
Scientist90s <yshethwala@gmail.com>
2022-08-10 14:28:43 -0700

    Calibration Updated

--- src/Calibration.py
+++ src/Calibration.py
@@ -23,83 +23,81 @@ def calibrate():
     left = GPIO.input(LEFT)                                        
     right = GPIO.input(RIGHT)
     
-    print(right)
-    print(left)
-    # GPIO.output(DIR,CCW)
-    # while(left==0):
-    #     GPIO.output(STEP, GPIO.HIGH)
-    #     sleep(delay)
-    #     GPIO.output(STEP, GPIO.LOW)
-    #     sleep(delay)
-    #     left = GPIO.input(LEFT)                                        
-    #     right = GPIO.input(RIGHT)
-
-    # GPIO.output(DIR, CW)
-    # while(right==0):
-    #     GPIO.output(STEP, GPIO.HIGH)
-    #     sleep(delay)
-    #     GPIO.output(STEP, GPIO.LOW)
-    #     sleep(delay)
-    #     left = GPIO.input(LEFT)                                        
-    #     right = GPIO.input(RIGHT)
-    #     steps = steps+1
-    #     print(right)
-        
-    # position = int(input("Enter Desired Position of Bed (in %) :"))
-
-    # desired_step = int(steps * (position / 100))
-
-    # GPIO.output(DIR,CCW)
-    # while not (steps==desired_step):
-    #     GPIO.output(STEP, GPIO.HIGH)
-    #     sleep(delay)
-    #     GPIO.output(STEP, GPIO.LOW)
-    #     sleep(delay)
-    #     left = GPIO.input(LEFT)                                        
-    #     right = GPIO.input(RIGHT)
-    #     steps = steps-1
-
-if __name__ == '__main__':
-    
-    while(1):
-    
-        left = GPIO.input(LEFT)                                        
-        right = GPIO.input(RIGHT)
-        
-        print("Left",left)
-        print("Right",right)  
-        print("\n")  
-        
+    GPIO.output(DIR,CCW)
+    while(left==0):
         GPIO.output(STEP, GPIO.HIGH)
         sleep(delay)
         GPIO.output(STEP, GPIO.LOW)
         sleep(delay)
+        left = GPIO.input(LEFT)                                        
+        right = GPIO.input(RIGHT)
+
+    GPIO.output(DIR, CW)
+    while(right==0):
+        GPIO.output(STEP, GPIO.HIGH)
+        sleep(delay)
+        GPIO.output(STEP, GPIO.LOW)
+        sleep(delay)
+        left = GPIO.input(LEFT)                                        
+        right = GPIO.input(RIGHT)
+        steps = steps+1
+        print(right)
         
-        if(right == 1 or left == 1):
-            break
+    position = int(input("Enter Desired Position of Bed (in %) :"))
 
-    # file = open('Parameters.csv')
-    # type(file)
-    # csvreader = csv.reader(file)
-    # rows=[]
-    # for row in csvreader:
-    #     rows.append(row)
+    desired_step = int(steps * (position / 100))
 
-    # for i in range(0,len(rows)):
-    #     if(rows[i][0]=="LEFT"):
-    #         LEFT = int(rows[i][1])
-    #     if(rows[i][0]=="RIGHT"):
-    #         RIGHT = int(rows[i][1])
-    #     if(rows[i][0]=="DIR"):
-    #         DIR = int(rows[i][1])
-    #     if(rows[i][0]=="STEP"):
-    #         STEP = int(rows[i][1])
-    #     if(rows[i][0]=="Hub_Diameter"):
-    #         hub_dia = rows[i][1]
-    #     if(rows[i][0]=="step_angle"):
-    #         step_angle = rows[i][1]
+    GPIO.output(DIR,CCW)
+    while not (steps==desired_step):
+        GPIO.output(STEP, GPIO.HIGH)
+        sleep(delay)
+        GPIO.output(STEP, GPIO.LOW)
+        sleep(delay)
+        left = GPIO.input(LEFT)                                        
+        right = GPIO.input(RIGHT)
+        steps = steps-1
+
+if __name__ == '__main__':
     
-    #calibrate()
+    # while(1):
+    
+    #     left = GPIO.input(LEFT)                                        
+    #     right = GPIO.input(RIGHT)
+        
+    #     print("Left",left)
+    #     print("Right",right)  
+    #     print("\n")  
+        
+    #     GPIO.output(STEP, GPIO.HIGH)
+    #     sleep(delay)
+    #     GPIO.output(STEP, GPIO.LOW)
+    #     sleep(delay)
+        
+    #     if(right == 1 or left == 1):
+    #         break
+
+    file = open('Parameters.csv')
+    type(file)
+    csvreader = csv.reader(file)
+    rows=[]
+    for row in csvreader:
+        rows.append(row)
+
+    for i in range(0,len(rows)):
+        if(rows[i][0]=="LEFT"):
+            LEFT = int(rows[i][1])
+        if(rows[i][0]=="RIGHT"):
+            RIGHT = int(rows[i][1])
+        if(rows[i][0]=="DIR"):
+            DIR = int(rows[i][1])
+        if(rows[i][0]=="STEP"):
+            STEP = int(rows[i][1])
+        if(rows[i][0]=="Hub_Diameter"):
+            hub_dia = rows[i][1]
+        if(rows[i][0]=="step_angle"):
+            step_angle = rows[i][1]
+    
+    calibrate()
     GPIO.cleanup()
 
 	
